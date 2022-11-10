e334ee75d9bac11d194d50eab8210f3870c38542
AnilBamne <anilbamne20@gmail.com>
2022-11-10 13:33:38 +0530

    Adding CalculateLengthOfLineUC1 Code

--- LineComparisionComputation222Batch/LineCompare.cs
+++ LineComparisionComputation222Batch/LineCompare.cs
@@ -6,7 +6,22 @@ namespace LineComparisionComputation222Batch
     {
         static void Main(string[] args)
         {
+            //main
             Console.WriteLine("Welcome To LineComparisionComputation program");
+            /*UC1
+             * As a fan of geometry, I want to model a line based on a point
+             *consisting of (x, y) co-ordinates using the Cartesian system,So that 
+             *I can calculate its length.*/
+
+            Console.WriteLine("Enter the First co-ordinates x1 and y1 ");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            int y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second co-ordinates x2 and y2 ");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            int y2=Convert.ToInt32(Console.ReadLine());
+
+            double LineLength=Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of line is : "+ LineLength);
         }
     }
 }

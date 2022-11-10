71c6093f7afdea172eb46395830bd1bde97ce4d6
AnilBamne <anilbamne20@gmail.com>
2022-11-10 12:48:43 +0530

    Adding Welcome message into main branch

---
+++ LineComparisionComputation222Batch/LineCompare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionComputation222Batch
+{
+    internal class LineCompare
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To LineComparisionComputation program");
+        }
+    }
+}
